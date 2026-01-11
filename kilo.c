5e793d9e19a41de80cf9b8b001e16519c49498d3
prabir18 <prabirsingh1804@gmail.com>
2026-01-10 22:10:58 -0500

    welcome message

--- kilo.c
+++ kilo.c
@@ -147,8 +147,20 @@ void processKeypress() {
 
 void drawRows(struct abuf *ab) {
   for (int y = 0; y < E.screenrows; y++) {
-    abAppend(ab, "~", 1);
+    if (y == E.screenrows/4) {
+      char welcome[80];
 
+      int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor");
+      if (welcomelen > E.screencols) {
+        welcomelen = E.screencols;
+      }
+
+      abAppend(ab, welcome, welcomelen);
+    } else {
+      abAppend(ab, "~", 1);
+    }
+
+    abAppend(ab, "\x1b[K", 3);
     if (y < E.screenrows - 1) {
       abAppend(ab, "\r\n", 2);
     }
@@ -158,12 +170,13 @@ void drawRows(struct abuf *ab) {
 void refreshScreen() {
   struct abuf ab = ABUF_INIT;
 
-  abAppend(&ab, "\x1b[2J", 4);
-  abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25l", 6); // hide cursor
+  abAppend(&ab, "\x1b[H", 3); // cursor top left
 
   drawRows(&ab);
 
   abAppend(&ab, "\x1b[H", 3);
+  abAppend(&ab, "\x1b[?25h", 6); // unhide cursor
 
   write(STDOUT_FILENO, ab.b, ab.len);
   abFree(&ab);
